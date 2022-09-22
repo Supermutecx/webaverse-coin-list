72b8d5bd00a5bb939dd35b87266523dffa6bdf3a
Naoki Murano <naokimurano@outlook.com>
2022-09-23 04:34:32 +0900

    FE - DropDown for Currency Select

--- fullstack-web3/project/frontend/src/App.jsx
+++ fullstack-web3/project/frontend/src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import CoinList from "./components";
+import config from "./config/app";
+
 function App() {
+  const [currency, setCurrency] = useState(config.currencyList[0]);
+
   return (
-    <div className="App">
-    <h1>Hello Developer!</h1>
-        <p>Your code goes here.</p>
-    </div>
-  )
+    <Container className="mt-5">
+      <Navbar variant="dark" bg="dark" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#home">Currency :</Navbar.Brand>
+          <Navbar.Collapse id="navbar-dark-example">
+            <Nav>
+              <NavDropdown
+                title={currency}
+                menuVariant="dark"
+              >
+                {
+                  config.currencyList.map((item, id) => {
+                    return (<NavDropdown.Item onClick={() => {setCurrency(item)}} key={id}>{item}</NavDropdown.Item>);
+                  })
+                }
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <CoinList></CoinList>
+    </Container>
+  );
 }
 
-export default App
+export default App;
---
+++ fullstack-web3/project/frontend/src/config/app.js
@@ -0,0 +1,4 @@
+export default {
+    currencyList: ['USD', 'HKD', 'KRW', 'SGD'],
+    backendUrl: 'http://127.0.0.1:4000'
+}
