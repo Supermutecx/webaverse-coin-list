05ba8d090e6a0c50f3b22a07686d10c058fbea6b
Naoki Murano <naokimurano@outlook.com>
2022-09-23 18:36:03 +0900

    FE - Fetching exchange services(`final`)

--- fullstack-web3/project/backend/utils/utils.js
+++ fullstack-web3/project/backend/utils/utils.js
@@ -14,7 +14,7 @@ const fetchBestExchange = async (coinId, symbol, currency) => {
             "exchange": sortedData[0].exchange
         };
     } catch (error) {
-        return [];
+        return { "exchange": "No Exchange" };
     }
 }
 
@@ -26,7 +26,7 @@ const fetchSingleMarketList = async (symbol, currency) => {
         const resp = await fetchBestExchange(coin[0].id, symbol, currency);
         return resp;
     } catch (error) {
-        return [];
+        return { "exchange": "No Exchange" };
     }
 }
 
@@ -51,7 +51,7 @@ const fetchBatchMarketList = async (currency) => {
 
 exports.fetchCoinList = async (currency) => {
     const query = new URLSearchParams();
-    if(currency != undefined) query.append("currency", currency);
+    if (currency != undefined) query.append("currency", currency);
     const apiUrl = `${process.env.COIN_API}?${query.toString()}`;
     try {
         const apiResp = await axios.get(apiUrl);
@@ -63,7 +63,7 @@ exports.fetchCoinList = async (currency) => {
 
 exports.fetchMarketList = async (symbol, currency) => {
     try {
-        if(symbol == undefined) {
+        if (symbol == undefined) {
             const marketResp = await fetchBatchMarketList(currency);
             return marketResp;
         } else {
--- fullstack-web3/project/frontend/src/components/CoinList.jsx
+++ fullstack-web3/project/frontend/src/components/CoinList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Table, Container } from "react-bootstrap";
+import { Table, Container, Spinner } from "react-bootstrap";
 import config from "../config/app";
 import TwitterLogo from "../assets/twitter.png";
 import SiteLogo from "../assets/website.png";
@@ -8,8 +8,8 @@ function CoinList(props) {
   const currency = props.currency;
   const mode = props.mode;
   const [coinList, setCoinList] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [exchangeList, setExchangeList] = useState([]);
 
   const numberWithCommas = (x) => {
     return x?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -28,6 +28,7 @@ function CoinList(props) {
   }, [mode]);
 
   useEffect(() => {
+    setLoading(true);
     if (currency != null) {
       fetch(`${config.backendUrl}/coin-route?currency=${currency}`)
         .then((response) => {
@@ -44,7 +45,6 @@ function CoinList(props) {
           }
         })
         .catch((err) => {
-          setError(err.message);
           setCoinList([]);
         })
         .finally(() => {
@@ -52,6 +52,52 @@ function CoinList(props) {
         });
     }
   }, [currency]);
+
+  useEffect(async () => {
+    if (loading == false) {
+      for await (const coin of coinList) {
+        const existList = exchangeList.filter(
+          (item) => item.coinId == coin.id && item.currency == currency
+        );
+        if (existList.length == 0) {
+          const apiResp = await fetch(
+            `${config.backendUrl}/exchange-route?coinId=${coin.symbol}&currency=${currency}`
+          );
+          if (apiResp.status == 200) {
+            const jsonResp = await apiResp.json();
+
+            if (existList.length > 0) {
+              // Update if exist
+              exchangeList.map((item) => {
+                if (item.coinId == coin.id && item.currency == currency) {
+                  item.exchange = jsonResp.data.exchange;
+                }
+                return item;
+              });
+            } else {
+              // Push if not exist
+              exchangeList.push({
+                coinId: coin.id,
+                currency: currency,
+                exchange: jsonResp.data.exchange,
+              });
+            }
+            setExchangeList([...exchangeList]);
+          }
+        }
+      }
+    }
+  }, [loading]);
+
+  useEffect(() => {
+    const mergedList = coinList.map((itm) => ({
+      ...exchangeList.find(
+        (item) => item.coinId === itm.id && item.currency == currency && item
+      ),
+      ...itm,
+    }));
+    setCoinList(mergedList);
+  }, [exchangeList, currency]);
 
   return (
     <Container className="mt-3">
@@ -72,52 +118,54 @@ function CoinList(props) {
           </tr>
         </thead>
         <tbody>
-          {loading
-            ? "Loading"
-            : coinList.map((item, id) => {
-                return (
-                  <tr key={id}>
-                    <td>{id + 1}</td>
-                    <td>{item.rank}</td>
-                    <td>
+          {loading ? (
+            <tr>
+              <td>Loading</td>
+            </tr>
+          ) : (
+            coinList.map((item, id) => {
+              return (
+                <tr key={id}>
+                  <td>{id + 1}</td>
+                  <td>{item?.rank}</td>
+                  <td>
+                    <img
+                      src={item?.icon}
+                      className="img-rounded float-left w-sm-50"
+                      width="30px"
+                      alt=""
+                    />{" "}
+                    {item?.name} {item?.symbol}
+                  </td>
+                  <td>{numberWithCommas(item?.price.toFixed(2))}$</td>
+                  <td>{item?.priceChange1h}%</td>
+                  <td>{item?.priceChange1d}%</td>
+                  <td>{item?.priceChange1w}%</td>
+                  <td>{numberWithCommas(item?.marketCap?.toFixed(2))}</td>
+                  <td>{numberWithCommas(item?.volume?.toFixed(2))}</td>
+                  <td className="d-flex">
+                    <a href={item?.websiteUrl} target="_blank">
                       <img
-                        src={item.icon}
+                        src={SiteLogo}
                         className="img-rounded float-left w-sm-50"
                         width="30px"
-                        alt=""
-                      />{" "}
-                      {item.name} {item.symbol}
-                    </td>
-                    <td>{numberWithCommas(item.price.toFixed(2))}$</td>
-                    <td>{item.priceChange1h}%</td>
-                    <td>{item.priceChange1d}%</td>
-                    <td>{item.priceChange1w}%</td>
-                    <td>{numberWithCommas(item.marketCap?.toFixed(2))}</td>
-                    <td>{numberWithCommas(item.volume?.toFixed(2))}</td>
-                    <td className="d-flex">
-                      <a href={item.websiteUrl} target="_blank">
-                        <img
-                          src={SiteLogo}
-                          className="img-rounded float-left w-sm-50"
-                          width="30px"
-                          alt={item.name}
-                        />
-                      </a>{" "}
-                      <a href={item.twitterUrl} target="_blank">
-                        <img
-                          src={TwitterLogo}
-                          className="img-rounded float-left w-sm-50"
-                          width="30px"
-                          alt={item.name}
-                        />
-                      </a>
-                    </td>
-                    <td>
-                        Binance
-                    </td>
-                  </tr>
-                );
-              })}
+                        alt={item?.name}
+                      />
+                    </a>{" "}
+                    <a href={item?.twitterUrl} target="_blank">
+                      <img
+                        src={TwitterLogo}
+                        className="img-rounded float-left w-sm-50"
+                        width="30px"
+                        alt={item?.name}
+                      />
+                    </a>
+                  </td>
+                  <td>{item?.exchange ? item?.exchange : (<Spinner animation="border" role="status"/>)}</td>
+                </tr>
+              );
+            })
+          )}
         </tbody>
       </Table>
     </Container>
